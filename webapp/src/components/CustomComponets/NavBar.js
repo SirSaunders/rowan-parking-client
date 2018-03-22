7d9c6cba500752719c9a744562333d33aa17edd5
Johnathan Saunders <Saundersokj@gmail.com>
2018-03-22 15:47:15 -0400

    updated navbar to be dynamic with links then move to hamburger

--- webapp/src/components/CustomComponets/NavBar.js
+++ webapp/src/components/CustomComponets/NavBar.js
@@ -3,11 +3,14 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import NavBarBtns from './NavbarBtns';
+
 
 export default class NavBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {open: false};
+        this.state = {open: false, width: window.innerWidth };
         this.handleToggle = this.handleToggle.bind(this);
     }
 
@@ -15,27 +18,51 @@ export default class NavBar extends React.Component {
         this.setState({open: !this.state.open});
     }
 
-
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener("resize", this.updateDimensions.bind(this));
+    }
+    updateDimensions() {
+            this.setState({ width: window.innerWidth });
+    }
     render () {
         return (
             <div>
-                <AppBar
-                    title={this.props.title}
-                    iconClassNameRight="muidocs-icon-navigation-expand-more"
-                    onLeftIconButtonClick={()=>this.handleToggle()}
-                />
-                <Drawer docked={false}
-                        width={200}
-                        open={this.state.open}
-                        onRequestChange={(open) => this.setState({open})}
-                >
-                    {( this.props.menuItems != null &&  this.props.menuItems.length > 0)?
+                {(this.state.width < 650)?
+                    <div>
+                           <AppBar
+                            title={this.props.title}
+                            iconClassNameRight="muidocs-icon-navigation-expand-more"
+                            onLeftIconButtonClick={()=>this.handleToggle()}
 
-                        this.props.menuItems.map((item, index) => (
-                            <MenuItem onClick={() => window.location.assign(item.path)}>{item.title}</MenuItem>
-                        )) : 'null'
-                    }
-                </Drawer>
+
+                        />
+                        <Drawer docked={false}
+                                width={200}
+                                open={this.state.open}
+                                onRequestChange={(open) => this.setState({open})}
+                        >
+                            {( this.props.menuItems != null &&  this.props.menuItems.length > 0)?
+                                this.props.menuItems.map((item, index) => (
+                                    <MenuItem onClick={() => window.location.assign(item.path)}>{item.title}</MenuItem>
+                                )) : 'null'
+                            }
+
+                        </Drawer>
+                   </div>
+                    :
+                    <AppBar
+                        title={this.props.title}
+                        iconElementLeft={<div></div>}
+
+                        iconElementRight={<NavBarBtns
+                            menuItems = {this.props.menuItems}
+                        />}
+
+                    />
+                }
+
+
             </div>
         )
     }
