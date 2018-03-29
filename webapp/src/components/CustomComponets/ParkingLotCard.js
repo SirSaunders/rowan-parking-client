df2beac1ef844512f3e1db64c7ce5433e46d9def
Johnathan Saunders <Saundersokj@gmail.com>
2018-03-29 15:13:22 -0400

    added parking lot card component stub

---
+++ webapp/src/components/CustomComponets/ParkingLotCard.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import NavBarBtns from './NavbarBtns';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+export default class ParkingLotCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render () {
+        return (
+            <div>
+
+                <Card>
+                <CardMedia
+                    overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+                >
+                    <img src="images/nature-600-337.jpg" alt="" />
+                </CardMedia>
+                <CardTitle title="Card title" subtitle="Card subtitle" />
+                <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                </CardText>
+                <CardActions>
+                    <FlatButton label="Action1" />
+                    <FlatButton label="Action2" />
+                </CardActions>
+            </Card>
+
+            </div>
+        )
+    }
+}
