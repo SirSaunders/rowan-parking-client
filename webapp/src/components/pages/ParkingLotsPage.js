aff0a40259d4b4bd97eec2108354f45d6a974082
ZaranaP <parmar67@students.rowan.edu>
2018-04-07 16:18:40 -0400

    added component to display from/to bar

--- webapp/src/components/pages/ParkingLotsPage.js
+++ webapp/src/components/pages/ParkingLotsPage.js
@@ -1,24 +1,24 @@
 
 import React, { Component } from 'react';
 import {Router, route, indexRoute, hashHistory} from "react-router"
+import DatePicker from 'material-ui/DatePicker';
+import TimePicker from 'material-ui/TimePicker';
 import '../../App.css';
-
-
-
+import MenuItem from "material-ui/MenuItem/index";
+import NavBarBtns from "../CustomComponets/NavbarBtns";
+import AppBar from "material-ui/AppBar/index";
+import FromToBar from '../CustomComponets/FromToBar'
 
 export default class ParkingLotsPage extends React.Component {
     constructor(props) {
         super(props);
-
     }
 
     render() {
 
-
         return <div className="App">
-                Parking lots page
-            </div>
-
+            <FromToBar/>
+        </div>
 
     }
 }
