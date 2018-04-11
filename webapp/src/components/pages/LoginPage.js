7691e5fcb37f15f8e05625ff6c79904dc7e0a83d
Johnathan Saunders <Saundersokj@gmail.com>
2018-04-11 13:10:28 -0400

    removed bower and index.html stuff not using firbase ui

--- webapp/src/components/pages/LoginPage.js
+++ webapp/src/components/pages/LoginPage.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import {Router, route, indexRoute, hashHistory} from "react-router"
 import '../../css/App.css';
+import * as firebase from 'firebase';
 
 
 
@@ -11,6 +12,53 @@ export default class LoginPage extends React.Component {
         super(props);
 
     }
+    componentDidMount(){
+        // firebase.auth().getRedirectResult().then(function(result) {
+        //     if (result.credential) {
+        //         // This gives you a Google Access Token. You can use it to access the Google API.
+        //         var token = result.credential.accessToken;
+        //         // ...
+        //     }
+        //     // The signed-in user info.
+        //     var user = result.user;
+        // }).catch(function(error) {
+        //     // Handle Errors here.
+        //     var errorCode = error.code;
+        //     var errorMessage = error.message;
+        //     // The email of the user's account used.
+        //     var email = error.email;
+        //     // The firebase.auth.AuthCredential type that was used.
+        //     var credential = error.credential;
+        //     // ...
+        // });
+        this.signIn()
+    }
+
+    signIn(){
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        firebase.auth().useDeviceLanguage();
+        //firebase.auth().signInWithRedirect(provider);
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log((result))
+            // ...
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+
+    }
+
 
     render() {
 
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import  Routes from './Routes'
 import Layout from './components/Layout';
+import * as firebase from 'firebase';
 
-
+var config = {
+    apiKey: "AIzaSyDBv2kcDm1vDMkq03rMQ-75DiLp4vUFFLI",
+    authDomain: "rowanparking-b71d8.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
     <Layout/>,
