d735e0b6d6ccf050db4fd2f8da55c5195f76afc2
Johnathan Saunders <Saundersokj@gmail.com>
2018-04-11 14:09:32 -0400

    passed google login in stuff in to a signin componet with, to sign in just add the button and the state listener. refer to login page

--- webapp/src/components/pages/LoginPage.js
+++ webapp/src/components/pages/LoginPage.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import {Router, route, indexRoute, hashHistory} from "react-router"
 import '../../css/App.css';
 import * as firebase from 'firebase';
+import SignInBtn from '../CustomComponets/SignInBtn'
 
 
 
@@ -13,50 +14,10 @@ export default class LoginPage extends React.Component {
 
     }
     componentDidMount(){
-        // firebase.auth().getRedirectResult().then(function(result) {
-        //     if (result.credential) {
-        //         // This gives you a Google Access Token. You can use it to access the Google API.
-        //         var token = result.credential.accessToken;
-        //         // ...
-        //     }
-        //     // The signed-in user info.
-        //     var user = result.user;
-        // }).catch(function(error) {
-        //     // Handle Errors here.
-        //     var errorCode = error.code;
-        //     var errorMessage = error.message;
-        //     // The email of the user's account used.
-        //     var email = error.email;
-        //     // The firebase.auth.AuthCredential type that was used.
-        //     var credential = error.credential;
-        //     // ...
-        // });
-        this.signIn()
-    }
-
-    signIn(){
-        var provider = new firebase.auth.GoogleAuthProvider();
-        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-        firebase.auth().useDeviceLanguage();
-        //firebase.auth().signInWithRedirect(provider);
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            console.log((result))
-            // ...
-        }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
+        //listener for login state change
+        firebase.auth().onAuthStateChanged(function (user){
+            console.log(user)
         });
-
     }
 
 
@@ -64,9 +25,16 @@ export default class LoginPage extends React.Component {
 
 
         return <div className="App">
-            <div id="firebaseui-auth-container"></div>
-
-                Login page
+            <div style={{"padding": 100}}>
+            <SignInBtn
+                text={"Login and Reserve Today!"}
+                color={"Red"}
+                textColor={"White"}
+                width={250}
+                height ={50}
+                fontSize={15}
+                />
+                </div>
             </div>
 
 
