10f5d90ebd24d3017020a0b208b1f54f6dfed984
Johnathan Saunders <Saundersokj@gmail.com>
2018-04-11 23:56:13 -0400

    added list view

--- mobileApp/components/pages/ParkingLotsPage.js
+++ mobileApp/components/pages/ParkingLotsPage.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { Button, View, Text } from 'react-native';
 
-
+import { AppRegistry, FlatList, StyleSheet, TouchableOpacity, Image} from 'react-native';
 
 export default class ParkingLotsPage extends React.Component {
     constructor(props) {
@@ -13,14 +13,47 @@ export default class ParkingLotsPage extends React.Component {
         title: 'Parking Lots Page',
     };
 
+    getData(){
+        return [
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+        ]
+    }
+
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Parking lotsr Screen</Text>
-                <Button
-                title="Pick a lot"
-                onPress={() => this.props.navigation.navigate('ConfirmationPage')}
-                />
+                <View style={styles.container}>
+                    <FlatList
+                        data={this.getData()}
+                        renderItem={
+                            ({item}) =>
+                                <TouchableOpacity onPress={() => this.props.navigation.navigate('ConfirmationPage')}>
+                                    <View style={styles.itemContainer}>
+                                        <Image
+                                            style={{width: "100%", height: 200}}
+                                            source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+                                        />
+                                        <Text style={styles.item}>{item.key}</Text>
+
+                                    </View>
+                                    <View
+                                        style={{
+                                            borderBottomColor: 'black',
+                                            borderBottomWidth: 1,
+                                        }}
+                                    />
+                                </TouchableOpacity>
+
+                        }
+                    />
+                </View>
             </View>
         );
     }
@@ -28,3 +61,19 @@ export default class ParkingLotsPage extends React.Component {
 }
 
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width:"100%",
+    },
+    itemContainer:{paddingLeft: 10,
+        paddingRight:10,
+        paddingTop: 10
+    },
+    item: {
+        textAlign: 'center',
+        fontSize: 18,
+        height: 44,
+
+    },
+})
