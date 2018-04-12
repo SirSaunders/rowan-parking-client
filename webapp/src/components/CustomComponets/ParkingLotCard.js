9b49c4a577a9ecea41c862de6bcfe393464ef425
agunter01 <a.gunter01@gmail.com>
2018-04-12 16:03:22 -0400

    Card is now clickable.

--- webapp/src/components/CustomComponets/ParkingLotCard.js
+++ webapp/src/components/CustomComponets/ParkingLotCard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
+import '../../css/ParkingLotCard.css';
+
 export default class ParkingLotCard extends React.Component {
     constructor(props) {
         super(props);
@@ -11,26 +13,22 @@ export default class ParkingLotCard extends React.Component {
     render () {
 
 
-        const style = {
-            width: '80%',
-            marginTop:'50px',
-            marginLeft: "auto",
-            marginRight: "auto"
-        };
+
 
 
         return (
-            <div style={style}>
-                <Card >
-                    <CardMedia overlay={<CardTitle title={'Monkey'} subtitle={'Close up'} />}>
-                        <img className="Crop-Fit" height='400px'  src={'http://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg'} alt="" />
-                    </CardMedia>
-                    <CardText>
-                        {'this is a monkey woo'}
-                    </CardText>
-                    <CardActions>
-                        <FlatButton label={'click me'} onClick={()=>alert('clicked')}/>
-                    </CardActions>
+            <div className="Card">
+                <Card onClick={ ()=>alert('Congratulations on attempting to make a reservation! Unfortunately this monkey does not allow reservations at this time. Please check back later!')}>
+                        <CardMedia overlay={<CardTitle title={'Monkey'} subtitle={'Close up'} />}>
+                            <img className="Crop-Fit" height='400px'  src={'http://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg'} alt="" />
+                        </CardMedia>
+                        <CardText>
+                            {'this is a monkey woo'}
+                        </CardText>
+                        <CardActions>
+                            <FlatButton label={'click me'} onClick={()=>alert('clicked')}/>
+
+                        </CardActions>
                 </Card>
             </div>
         )
