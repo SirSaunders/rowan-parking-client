682f883a99cc8687d0233c21aefd55e84937ff9d
takeshwari <kamalt4@students.rowan.edu>
2018-04-22 18:47:03 -0400

    get user info

--- webapp/src/components/pages/ProfilePage.js
+++ webapp/src/components/pages/ProfilePage.js
@@ -4,14 +4,32 @@ import {Router, route, indexRoute, hashHistory} from "react-router"
 
 import '../../css/App.css';
 import { Grid, InputLabel } from "material-ui-next"
-
+import * as firebase from 'firebase';
 
 
 
 import avatar from "../../utils/image.jpg";
 
+profile_info()
+{
+    GoogleSignInAccount
+    acct = GoogleSignIn.getLastSignedInAccount(getActivity());
+    if (acct != null) {
+        String
+        personName = acct.getDisplayName();
+        String
+        personGivenName = acct.getGivenName();
+        String
+        personFamilyName = acct.getFamilyName();
+        String
+        personEmail = acct.getEmail();
+        String
+        personId = acct.getId();
+        Uri
+        personPhoto = acct.getPhotoUrl();
+    }
 
-
+}
 
 export default class ProfilePage extends React.Component {
     constructor(props) {
