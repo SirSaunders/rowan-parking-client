9874b2c10f9380bfcad9e3ed0a0094b79ec7c66d
takeshwari <kamalt4@students.rowan.edu>
2018-04-22 18:15:47 -0400

    commented out line of code that is breaking the code

---
+++ webapp/src/components/CustomComponets/Profile/reservation_list.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react';
+import {Router, route, indexRoute, hashHistory} from "react-router"
+
+import '../../css/App.css';
+import { Grid, InputLabel } from "material-ui-next"
+
+
+
+
+import avatar from "../../utils/image.jpg";
+
+
+
+
+export default class ProfilePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+
+
+        return <div className="App">
+            profile page
+        </div>
+
+
+    }
+}
